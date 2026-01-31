bf32888a356d617d1aab62b432d5e74c85045a5d
Vivek <kandarivivek260@gmail.com>
2026-01-31 14:19:55 +0000

    added new methods

--- src/RebaseToken.sol
+++ src/RebaseToken.sol
@@ -28,7 +28,7 @@ contract RebaseToken is ERC20 {
     * @param _newInterestRate The new interest rate to be set
     * @dev The interest rate can only decrease
     */
-    function interestRate(uint256 _newInterestRate) external {
+    function setInterestRate(uint256 _newInterestRate) external {
         if (_newInterestRate >= s_interestRate) {
             revert RebaseToken__InterestRateCanOnlyDecrease(s_interestRate, _newInterestRate);
         }
@@ -36,6 +36,16 @@ contract RebaseToken is ERC20 {
         s_interestRate = _newInterestRate;
 
         emit InterestRateSet(_newInterestRate);
+    }
+
+    /*
+    * @notice get the principle balance of a user. this is the number of tokens that have currently been minted to the user, 
+    * not including any interest rate that has accured since last time the user interacted with protocol.
+    * @param _user the user to get pricipla balance for
+    * @returns the principle balance of the user 
+    */
+    function principleBalanceOf(address _user) external view returns (uint256){
+        return super.balanceOf(_user);
     }
 
     /*
@@ -51,6 +61,19 @@ contract RebaseToken is ERC20 {
     }
 
     /*
+    * @notice burn the user tokens when they withdraw from the vault
+    * @param _from the user to burn the tokens from
+    * @param _amount the amount of tokens to burn
+    */
+    function burn(address _from, uint256 _amount) external{
+        if(_amount == type(uint256).max){
+            _amount = balanceOf(_from);
+        }
+        _mintAccruedInterest(_from);
+        _burn(_from, _amount);
+    }
+
+    /*
     * @notice Calculate the balance for user including accumulated interest for a user since their last update
     * (principal balnace) + some interest
     * @param _user The user to calculate the balance for
@@ -58,6 +81,48 @@ contract RebaseToken is ERC20 {
     */
     function balanceOf(address _user) public view override returns (uint256) {
         return super.balanceOf(_user) * _calculateUserAccumulatedInterestSinceLastUpdate(_user) / PRECISION_FACTOR;
+    }
+
+    /*
+    * @notice Transfer tokens from one user to another
+    * @param _recipient the user to transfer the tokens to
+    * @param _amount the amount of tokens to transfer
+    * @returns True if the transfer was successful
+    */
+
+    function transfer(address _recipient, uint256 _amount) public override returns(bool){
+        _mintAccruedInterest(msg.sender);
+        _mintAccruedInterest(_recipient);
+        if(_amount == type(uint256).max){
+            _amount = balanceOf(msg.sender);
+        }
+
+        if(balanceOf(_recipient)==0){
+            s_userInterestRate[_recipient] = s_userInterestRate[msg.sender];
+        }
+
+        return super.transfer(_recipient, _amount);
+    }
+
+    /*
+    * @notice tranfer tokens form one user to another
+    *  @param _sender to transfer the tokens from
+    *  @param _recipient  to transfer the tokens to
+    * @param _amount the amount of tokens to transfer
+    * @return true if tranfer successful
+    */
+    function transferFrom(address _sender, address _recipient, uint256 _amount) public override returns (bool){
+        _mintAccruedInterest(_sender);
+        _mintAccruedInterest(_recipient);
+        if(_amount == type(uint256).max){
+            _amount = balanceOf(_sender);
+        }
+
+        if(balanceOf(_recipient)==0){
+            s_userInterestRate[_recipient] = s_userInterestRate[_sender];
+        }
+
+        return super.transferFrom(_sender,_recipient, _amount);
     }
 
     /*
@@ -71,8 +136,21 @@ contract RebaseToken is ERC20 {
         
     }
 
+    /*
+    * @notice Mint the accured interest to the user since the last time they interacted with the protocol (e.g. burn, mint, transfer)
+    *  @param the user to mint accured interest to
+    */
+
     function _mintAccruedInterest(address _user) internal {
+        uint256 previousPrincipleBalance = super.balanceOf(_user);
+        uint256 currentBalance = balanceOf(_user);
+        uint256 balanceIncrease = currentBalance - previousPrincipleBalance; 
         s_userLastUpdatedTimestamp[_user] = block.timestamp;
+        _mint(_user, balanceIncrease);
+    }
+
+    function getInterestRate() external view returns(uint256){
+        return s_interestRate;
     }
 
     /*
